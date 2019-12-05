0a2290698214fe24bff488fc74491a00725a99fc
prabhusundram <57883688+prabhusundram@users.noreply.github.com>
2019-12-05 20:02:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';  
+import ReactTable from "react-table";  
+//import "react-table/react-table.css";  
+  
+class App extends Component {  
+  render() {  
+     const data = [{  
+        name: 'Ayaan',  
+        age: 26  
+        },{  
+         name: 'Ahana',  
+         age: 22  
+         },{  
+         name: 'Peter',  
+         age: 40      
+         },{  
+         name: 'Virat',  
+         age: 30  
+         },{  
+         name: 'Rohit',  
+         age: 32  
+         },{  
+         name: 'Dhoni',  
+         age: 37  
+         }]  
+     const columns = [{  
+       Header: 'Name',  
+       accessor: 'name'  
+       },{  
+       Header: 'Age',  
+       accessor: 'age'  
+       }]  
+    return (  
+          <div>  
+              <table> 
+                  data={data}  
+                  columns={columns}  
+                  defaultPageSize = {2}  
+                  pageSizeOptions = {[2,4, 6]}  
+                  </table> 
+          </div>        
+    )  
+  }  
+}  
+
+export default App;
